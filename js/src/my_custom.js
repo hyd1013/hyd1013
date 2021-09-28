5bc875fd5a55d82a3fc5e2a2516a09fd877a0acb
hyd1013 <huangyaodong@xmbdqn.com>
2021-09-28 14:22:16 +0800

    Site updated: 2021-09-28 14:22:15

--- js/src/my_custom.js
+++ js/src/my_custom.js
@@ -1,6 +1,6 @@
 $(function(){
     let path = window.location.pathname;
-    if(path != "/" && path != "/archives/" && path != "/tags/"){
+    if(path != "/" && path != "/archives/" && path.indexOf("/tags/") < 0){
       $(".main-inner").addClass("main-width");
     }else{
       $(".main-inner").removeClass("main-width");
