cd735a1bf58a1ed3071d1526e8284c9851f66dfb
hyd1013 <huangyaodong@xmbdqn.com>
2021-10-14 09:02:44 +0800

    Site updated: 2021-10-14 09:02:44

--- js/src/my_custom.js
+++ js/src/my_custom.js
@@ -1,6 +1,6 @@
 $(function(){
     let path = window.location.pathname;
-    if(path != "/" && path != "/archives/" && path.indexOf("/tags/") < 0){
+    if(path != "/" && path != "/archives/" && path.indexOf("/tags/") < 0 && path.indexOf("/page/") < 0){
       $(".main-inner").addClass("main-width");
     }else{
       $(".main-inner").removeClass("main-width");
